0c55f5ba178c5db6e405f661e3f5bace8012af86
yaoFengFeng <1473922113@qq.com>
2019-05-16 06:55:30 +0800

    0516 06:55

---
+++ out/artifacts/unnamed/src/servlet/MySQLConnection.java
@@ -0,0 +1,41 @@
+package servlet;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class MySQLConnection {
+    public Connection getConnection(){
+        String driver = null;
+        String url = null;
+        String user = null;
+        String psd = null;
+        Connection conn = null;
+        Properties pro = new Properties();   //创建一个Propertise实例，用于从DBConnection中获取数据库连接参数
+        try {
+            pro.load(this.getClass().getClassLoader().getResourceAsStream("DBConfig.properties")); //加载DBConfig文件。
+            driver = pro.getProperty("driver");
+            url = pro.getProperty("url");
+            user = pro.getProperty("user");
+            psd = pro.getProperty("psd");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Class.forName(driver);   //加载jdbc驱动
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            conn = DriverManager.getConnection(url,user,psd); //获取数据库连接
+            conn.setAutoCommit(false);
+            return conn;       //返回一个数据库连接。
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
